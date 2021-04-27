9ffb820a00c1982350c67cefafc8b86b51f2be3d
lawrencecchen <md.lawrence.chen@gmail.com>
2021-04-27 13:37:27 -0700

    add vercel adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,5 @@
 const preprocess = require('svelte-preprocess');
+const vercel = require('@sveltejs/adapter-vercel');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -8,6 +9,7 @@ module.exports = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel({ out: 'public' })
 	}
 };
