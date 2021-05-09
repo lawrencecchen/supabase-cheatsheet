7bab0795f36150f1f258129063259993999f106c
lawrencecchen <md.lawrence.chen@gmail.com>
2021-05-08 20:26:35 -0700

    make navbar great

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,32 +1,41 @@
 <script lang="ts">
 	import 'tailwindcss/tailwind.css';
 	import '../app.css';
+	let y;
 </script>
 
 <svelte:head>
 	<title>Supabase Cookbook</title>
 </svelte:head>
 
-<div
-	class="w-full border-b shadow-sm px-4 py-3 flex items-center sticky top-0 backdrop-filter backdrop-blur firefox:bg-opacity-90 firefox:bg-white z-10"
->
-	<a class="font-semibold text-gray-700" href="/">Supabase Cheatsheet</a>
-	<div class="mr-0 ml-auto flex">
-		<a
-			href="https://github.com/lawrencecchen/supabase-cheatsheet"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="w-6 h-6"
-			><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-				><path
-					d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-				/></svg
-			></a
-		>
+<svelte:window bind:scrollY={y} />
+<div>
+	<div
+		class="w-full px-4 py-3 flex items-center fixed top-0 backdrop-filter backdrop-blur-sm backdrop-contrast-200 firefox:bg-opacity-95 firefox:bg-white z-10 transition duration-300 {y >
+		0
+			? 'border-b shadow-sm'
+			: ''}"
+	>
+		<a class="font-semibold text-gray-700" href="/">Supabase Cheatsheet</a>
+		<div class="mr-0 ml-auto flex">
+			<a
+				href="https://github.com/lawrencecchen/supabase-cheatsheet"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="w-6 h-6"
+				><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+					><path
+						d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+					/></svg
+				></a
+			>
+		</div>
+	</div>
+
+	<div class="mt-12">
+		<slot />
 	</div>
 </div>
-
-<slot />
 
 <style>
 	:global(.prose :is(h1, h2, h3, h4, h5, h6) > a) {
